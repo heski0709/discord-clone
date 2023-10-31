223215d71ee48713a6bec420eef6d1f65e3f886b
heski0709 <heski0709@gmail.com>
2023-10-31 15:39:40 +0900

    tailwind.config merge. create form  component. add middleware publicRoutes. add fileUpload Button

--- src/app/(setup)/page.tsx
+++ src/app/(setup)/page.tsx
@@ -1,23 +1,26 @@
-import { redirect } from "next/navigation"
+import InitialModal from "@/components/modals/Initial-modal";
 import { db } from "@/lib/db";
 import { initialProfile } from '@/lib/initial-profile';
+import { redirect } from "next/navigation";
 
 const SetupPage = async () => {
-    const profile = await initialProfile();
-    const server = await db.server.findFirst({
-        where: {
-            members: {
-                some: {
-                    profileId: profile.id
-                }
-            }
-        }
-    })
+    // const profile = await initialProfile();
+    // const server = await db.server.findFirst({
+    //     where: {
+    //         members: {
+    //             some: {
+    //                 profileId: profile.id
+    //             }
+    //         }
+    //     }
+    // })
 
-    if (server) {
-        return redirect(`/servers/${server.id}`)
-    }
+    // if (server) {
+    //     return redirect(`/servers/${server.id}`)
+    // }
     
-    return <div>Create a Server</div>;
+    return <InitialModal />;
 };
+
+
 export default SetupPage;
