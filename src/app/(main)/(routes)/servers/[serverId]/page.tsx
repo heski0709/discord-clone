27779e430ae3171557399e1b7a93cfb9b8e4fdfb
heski0709 <heski0709@gmail.com>
2023-11-03 16:16:58 +0900

    [feat] route channel, member page

--- src/app/(main)/(routes)/servers/[serverId]/page.tsx
+++ src/app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -1,6 +1,50 @@
-const ServerIdPage = () => {
-  return (
-    <div>ServerIdPage</div>
-  )
-}
-export default ServerIdPage
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+type ServerIdPageProps = {
+    params: {
+        serverId: string;
+    };
+};
+
+const ServerIdPage = async ({ params }: ServerIdPageProps) => {
+    const profile = await currentProfile();
+
+    if (!profile) {
+        return redirectToSignIn();
+    }
+
+    const server = await db.server.findUnique({
+        where: {
+            id: params.serverId,
+            members: {
+                some: {
+                    profileId: profile.id,
+                },
+            },
+        },
+        include: {
+            channels: {
+                where: {
+                    name: "general",
+                },
+                orderBy: {
+                    createAt: "asc",
+                },
+            },
+        },
+    });
+
+    const initialChannel = server?.channels[0];
+
+    if (initialChannel?.name !== "general") {
+        return null;
+    }
+
+    return redirect(
+        `/servers/${params.serverId}/channels/${initialChannel?.id}`
+    );
+};
+export default ServerIdPage;
